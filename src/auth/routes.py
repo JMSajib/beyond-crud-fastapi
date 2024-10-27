dcf4e8c5f4253f9ffe80df56c236fc517166c977
jahidul <jahidul@nexgencloud.com>
2024-10-27 12:20:52 +0600

    Added access token and refresh token and login endpoint

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,13 +1,17 @@
+from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import JSONResponse
 from sqlmodel.ext.asyncio.session import AsyncSession
 from src.db.main import get_session
-from src.auth.schemas import UserCreateModel, UserModel
+from src.auth.schemas import UserCreateModel, UserModel, UserLoginModel
 from src.auth.service import UserService
+from src.auth.utils import create_access_token, decode_token, verify_password
 
 auth_router = APIRouter()
 
 user_service = UserService()
 
+REFRESH_TOKEN_EXPIRY = 2
 
 @auth_router.post("/signup", response_model=UserModel, status_code=status.HTTP_201_CREATED)
 async def create_user(user_data: UserCreateModel, session: AsyncSession = Depends(get_session)):
@@ -19,3 +23,38 @@ async def create_user(user_data: UserCreateModel, session: AsyncSession = Depend
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with email already exist.")
     new_user = await user_service.create_user(user_data, session)
     return new_user
+
+@auth_router.post("/login")
+async def login_users(login_data: UserLoginModel, session: AsyncSession = Depends(get_session)):
+    email = login_data.email
+    password = login_data.password
+    user = await user_service.get_user_by_email(email, session)
+    if user is not None and verify_password(password, user.password_hash):
+        access_token = create_access_token(
+            user_data={
+                'email': user.email,
+                'user_uid': str(user.uid)
+            }
+        )
+        
+        refresh_token = create_access_token(
+            user_data={
+                'email': user.email,
+                'user_uid': str(user.uid)
+            },
+            refresh=True,
+            expiry=timedelta(days=REFRESH_TOKEN_EXPIRY)
+        )
+        
+        return JSONResponse(
+            content={
+                "message": "Login Successful",
+                "access_tooken": access_token,
+                "refresh_token": refresh_token,
+                "user": {
+                    "email": user.email,
+                    "user_uid": str(user.uid)
+                }
+            }
+        )
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid email or Password")
